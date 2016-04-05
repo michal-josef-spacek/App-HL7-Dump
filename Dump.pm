da1f1584c56bc3becc1290c5f3b21627243a2906
Michal Špaček <m.spacek@ibsmm.com>
2016-04-05 12:21:29 +0200

    Prepare for 0.03 version.

--- Dump.pm
+++ Dump.pm
@@ -13,7 +13,7 @@ use Net::HL7::Message;
 use Perl6::Slurp qw(slurp);
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -231,6 +231,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
