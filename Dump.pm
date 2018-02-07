4668aa6013dbbb753f7206a8343e46871807c0c3
Michal Josef Špaček <tupinek@gmail.com>
2018-02-07 08:06:47 +0100

    Prepare for 0.04 version

--- Dump.pm
+++ Dump.pm
@@ -10,7 +10,7 @@ use Getopt::Std;
 use Net::HL7::Message;
 use Perl6::Slurp qw(slurp);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -226,6 +226,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
